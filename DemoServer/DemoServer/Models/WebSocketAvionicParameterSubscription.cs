b460a9445172ac68a9cade89f577782dc4fde19a
Marco Schilder <marco.schilder@dlh.de>
2022-05-05 21:02:54 +0200

    Added Publish Profiles
    Added Profiles for Windowns_x64, Linux_x64, Linux_arm

---
+++ DemoServer/DemoServer/Models/WebSocketAvionicParameterSubscription.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json.Serialization;
+
+namespace DemoServer.Models
+{
+    public class WebSocketAvionicParameterSubscription
+    {
+        [JsonPropertyName("method")]
+        public string method { get; set; }
+
+        [JsonPropertyName("arguments")]
+        public SubscriptionArguments Arguments { get; set; }
+    }
+
+    public class SubscriptionArguments
+    {
+        [JsonPropertyName("type")]
+        public string Type { get; set; }
+
+        [JsonPropertyName("interval")]
+        public int? Interval { get; set; }
+
+        [JsonPropertyName("parameters")]
+        public string[] ParameterNames { get; set; }
+    }
+}
---
+++ DemoServer/DemoServer/Models/WebSocketResponse.cs
@@ -0,0 +1,40 @@
+﻿//
+// Copyright (c) Deutsche Lufthansa AG.
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+//
+
+using System.Text.Json.Serialization;
+
+namespace DemoServer.Models
+{
+    public class WebSocketResponse
+    {
+        [JsonPropertyName("returnCode")]
+        public string ReturnCode { get; set; }
+
+
+        [JsonPropertyName("reason")]
+        public string Reason { get; set; }
+
+        [JsonPropertyName("unknownParameters")]
+        public string[] UnKnownParameters { get; set; }
+    }
+
+    public enum WebSocketResponseCode
+    {
+        Ok,
+        Error
+    }
+
+    public enum WebSocketSubscriptionErrorCode
+    {
+        TimeOut = 1002,
+        BadFormat = 1007,
+        BadRequest = 1008,
+        TooLarge = 1009,
+        InvalidPeriod = 4010,
+        AllunknownParameters = 4011
+    }
+}
